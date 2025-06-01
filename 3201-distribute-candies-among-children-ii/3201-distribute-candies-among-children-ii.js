cdd24c82a8a63ab90bde62b49afcdfb156c5e28a
Naeem Akram <153123217+NaeemAbdullahAkram@users.noreply.github.com>
2025-06-01 10:25:18 +0500

    Time: 110 ms (33.33%), Space: 59.6 MB (16.67%) - LeetHub

---
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.js
@@ -0,0 +1,12 @@
+var distributeCandies = function(n, limit) {
+  let count = 0;
+  const m = Math.min(limit, n);
+  for (let i = 0; i <= m; i++) {
+    const remaining = n - i;
+    const maxPair = Math.min(limit, remaining);
+    const minPair = remaining - maxPair;
+    if (minPair > limit) continue;
+    count += maxPair - minPair + 1;
+  }
+  return count;
+};
